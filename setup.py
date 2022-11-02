061a571ddfe53fc0edce0ae0dc8468c3508aa407
iratigaritano <64250847+iratigaritano@users.noreply.github.com>
2022-11-02 08:42:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'bookish',         # How you named your package folder (MyLib)
   packages = ['bookish'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Irati Garitano',                   # Type in your name
-  author_email = 'javier.ortega@alumni.mondragon.edu',    # Type in your E-Mail
+  author_email = 'irati.garitano@alumni.mondragon.edu',    # Type in your E-Mail
   url = 'https://github.com/iratigaritano/bookish',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/iratigaritano/bookish/archive/refs/tags/v01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/iratigaritano/bookish/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['WORD', 'COUNT', 'BOOK'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'epub_conversion',
